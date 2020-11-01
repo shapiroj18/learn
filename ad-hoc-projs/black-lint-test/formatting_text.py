b12001f2a60cd96070ff80231aac45f67f8e33cf
Jonathan Shapiro <jonathanshapiro@jonathans-mbp.home>
2020-11-01 17:22:52 -0500

    added black-lint-test

---
+++ ad-hoc-projs/black-lint-test/formatting_text.py
@@ -0,0 +1,22 @@
+import pandas as pd
+
+list = ['hello', 'world',
+'enter',
+'other enter']
+
+
+def make_fun(test1, test2, test3
+    ):
+    '''this adds the tests
+
+    '''
+
+
+    return test1 + test2 + test3
+
+
+
+
+print(list)
+
+print(make_fun(1, 2, 3))
