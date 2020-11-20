82e150a1e21a2422d49cf56bc382c8900f2d4d2d
Jonathan Shapiro <jonathanshapiro@Jonathans-MacBook-Pro.local>
2020-11-19 20:48:15 -0500

    black updates

--- ad-hoc-projs/black-lint-test/formatting_text.py
+++ ad-hoc-projs/black-lint-test/formatting_text.py
@@ -1,17 +1,12 @@
 import pandas as pd
 
-list = ["Jonathan", "Shivali", 
-
-"Jay"]
+list = ["Jonathan", "Shivali", "Jay"]
 
 
 def function_test(var_1=None, var_2=1):
+    pass
 
-<<<<<<< HEAD
 
-print( list)
+print(list)
 
 print(make_fun(1, 2, 3))
-=======
-    pass
->>>>>>> 8d0b90f81d6666cf1971810c7e5f08d2be499209
