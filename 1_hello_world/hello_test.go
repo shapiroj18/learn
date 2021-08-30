e81caa047dd3a5b09b71c84d6dc56cefcd6b3adc
Jonathan Shapiro <shapiroj18@gmail.com>
2021-08-30 14:07:11 -0400

    renaming folders for sorting

---
+++ 1_hello_world/hello_test.go
@@ -0,0 +1,40 @@
+package main
+
+import "testing"
+
+func TestHello(t *testing.T) {
+
+	assertCorrectMessage := func(t testing.TB, got, want string) {
+		t.Helper()
+		if got != want {
+			t.Errorf("got %q want %q", got, want)
+		}
+	}
+
+	t.Run("saying hello to people", func(t *testing.T) {
+
+		got := Hello("Jonathan", "")
+		want := "Hello, Jonathan"
+
+		assertCorrectMessage(t, got, want)
+	})
+
+	t.Run("say 'Hello, World' when an empty string is supplied", func(t *testing.T) {
+		got := Hello("", "")
+		want := "Hello, World"
+
+		assertCorrectMessage(t, got, want)
+	})
+
+	t.Run("in Spanish", func(t *testing.T) {
+		got := Hello("Brenda", "Spanish")
+		want := "Hola, Brenda"
+		assertCorrectMessage(t, got, want)
+	})
+
+	t.Run("in French", func(t *testing.T) {
+		got := Hello("Logan", "French")
+		want := "Bonjour, Logan"
+		assertCorrectMessage(t, got, want)
+	})
+}
