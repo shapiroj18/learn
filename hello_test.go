50adbfca53643e62d7f1751805f172075d388053
Jonathan Shapiro <shapiroj18@gmail.com>
2021-08-30 13:24:09 -0400

    hello world additional testing func

--- hello_test.go
+++ hello_test.go
@@ -3,10 +3,26 @@ package main
 import "testing"
 
 func TestHello(t *testing.T) {
-	got := Hello("Jonathan")
-	want := "Hello, Jonathan"
 
-	if got != want {
-		t.Errorf("got %q want %q", got, want)
+	assertCorrectMessage := func(t testing.TB, got, want string) {
+		t.Helper()
+		if got != want {
+			t.Errorf("got %q want %q", got, want)
+		}
 	}
+
+	t.Run("saying hello to people", func(t *testing.T) {
+
+		got := Hello("Jonathan")
+		want := "Hello, Jonathan"
+
+		assertCorrectMessage(t, got, want)
+	})
+
+	t.Run("say 'Hello, World' when an empty string is supplied", func(t *testing.T) {
+		got := Hello("")
+		want := "Hello, World"
+
+		assertCorrectMessage(t, got, want)
+	})
 }
