b187799b90879c1d59997955a4a35d5e89185110
Jonathan Shapiro <shapiroj18@gmail.com>
2021-12-02 09:27:46 -0500

    added answer to part 2

--- advent-of-code/2021/day_1/main.py
+++ advent-of-code/2021/day_1/main.py
@@ -21,8 +21,26 @@ def answer_increase_decrease_1(data_floor_depth: list):
     
     return answers
 
+def answer_increase_decrease_2(data_floor_depth: list):
+    
+    answers = []
+    for i, x in enumerate(data_floor_depth):
+        if i == 0:
+            answers.append('(N/A - no previous measurement)')
+        elif i < len(data_floor_depth) - 2:
+            sum_last = sum([data_floor_depth[i - 1], data_floor_depth[i], data_floor_depth[i+1]])
+            sum_current = sum([data_floor_depth[i], data_floor_depth[i+1], data_floor_depth[i+2]])
+            
+            if sum_current > sum_last:
+                answers.append('(increased)')
+            elif sum_current == sum_last:
+                answers.append('(no change)')
+            else:
+                answers.append('(decreased)')
+                
+    return answers
 
-def count_increases_1(answers: list) -> int:
+def count_increases(answers: list) -> int:
     
     total = answers.count('(increased)')
     
@@ -32,10 +50,18 @@ def main_1():
     
     raw_data = read_file('data.txt')
     answers = answer_increase_decrease_1(raw_data)
-    total_count = count_increases_1(answers)
+    total_count = count_increases(answers)
     
     return total_count
+
+def main_2():
     
+    raw_data = read_file('data.txt')
+    answers = answer_increase_decrease_2(raw_data)
+    total_count = count_increases(answers)
+    
+    return total_count
 
 if __name__ == '__main__':
-    exit(print(main_1()))
+    print(f'The answer to question 1: {main_1()}')
+    print(f'The answer to question 2: {main_2()}')
