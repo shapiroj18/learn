91d8797bf41da696a034f9f976e5e42388c19eb6
Jonathan Shapiro <shapiroj18@gmail.com>
2021-12-01 19:30:08 -0500

    got first answer correct

---
+++ advent-of-code/2021/day_1/main.py
@@ -0,0 +1,41 @@
+def read_file(file_path: str) -> list:
+    f = open(file_path)
+    content = f.read().splitlines()
+    
+    data = []
+    for line in content:
+        data.append(int(line))
+        
+    return data
+
+def answer_increase_decrease(data_floor_depth: list):
+    
+    answers = []
+    for i, x in enumerate(data_floor_depth):
+        if i == 0:
+            answers.append('(N/A - no previous measurement)')
+        elif data_floor_depth[i] > data_floor_depth[i-1]:
+            answers.append('(increased)')
+        else:
+            answers.append('(decresed)')
+    
+    return answers
+
+
+def count_increases(answers: list) -> int:
+    
+    total = answers.count('(increased)')
+    
+    return total
+
+def main():
+    
+    raw_data = read_file('data.txt')
+    answers = answer_increase_decrease(raw_data)
+    total_count = count_increases(answers)
+    
+    return total_count
+    
+
+if __name__ == '__main__':
+    exit(print(main()))
