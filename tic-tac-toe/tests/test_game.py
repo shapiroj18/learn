59c21c9e54e761630dcde4dffe07696f2287f24c
Jonathan Shapiro <shapiroj18@gmail.com>
2021-12-22 11:04:00 -0500

    formatting updates

--- tic-tac-toe/tests/test_game.py
+++ tic-tac-toe/tests/test_game.py
@@ -14,37 +14,41 @@ def test_game_setup(monkeypatch):
 def test_game_setup(monkeypatch):
     monkeypatch.setattr("builtins.input", lambda _: "computer")
     assert game.game_setup() == "computer"
-    
+
+
 def test_get_names(monkeypatch):
     monkeypatch.setattr("builtins.input", lambda _: "Jonathan")
     assert game.get_names("computer") == ("Jonathan", "computer")
-    
+
+
 def test_get_elements(monkeypatch):
     monkeypatch.setattr("builtins.input", lambda _: "x")
     assert game.get_elements("j", "k", "human") == ("X", "O")
 
+
 def test_get_elements(monkeypatch):
     monkeypatch.setattr("builtins.input", lambda _: "x")
     assert game.get_elements("j", "computer", "computer") == ("X", "O")
-    
+
+
 def test_make_move(monkeypatch):
-    
+
     monkeypatch.setattr("builtins.input", lambda _: "1,3")
 
     assert game.make_move(
-        current_board = {
-        "row_1_col_1": "X",
-        "row_1_col_2": "X",
-        "row_1_col_3": None,
-        "row_2_col_1": "O",
-        "row_2_col_2": "O",
-        "row_2_col_3": None,
-        "row_3_col_1": None,
-        "row_3_col_2": None,
-        "row_3_col_3": None,
-    },
-    player = "j",
-    element = "X"
+        current_board={
+            "row_1_col_1": "X",
+            "row_1_col_2": "X",
+            "row_1_col_3": None,
+            "row_2_col_1": "O",
+            "row_2_col_2": "O",
+            "row_2_col_3": None,
+            "row_3_col_1": None,
+            "row_3_col_2": None,
+            "row_3_col_3": None,
+        },
+        player="j",
+        element="X",
     ) == {
         "row_1_col_1": "X",
         "row_1_col_2": "X",
